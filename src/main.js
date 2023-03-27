a9101cb9a0a82f010222e74ac5b2a6502e013ec0
Jamie Meyer <45072324+HeatXD@users.noreply.github.com>
2023-03-27 05:21:24 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -14,7 +14,7 @@ Room Full: &!RF
 No Room Found: &!NRF
 Room Closed: &!RC
 Room Code Response: &!RCR&#{room_code} -> e.g. &!RCR&#AAAABBBBCCCCDDDD
-Room Info Exchange: &!RIE&#{full_address}&#{external_data} -> e.g. &!RIE&#127.0.0.1&#4444&#HEATXD
+Room Info Exchange: &!RIE&#{full_address}&#{external_data} -> e.g. &!RIE&#127.0.0.1:4444&#HEATXD
 ----------------------------------
 */
 // acquire env vars
